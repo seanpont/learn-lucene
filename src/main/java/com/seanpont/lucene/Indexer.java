66efbfafef111d992cf2d2f9fcd991e524e50c1f
Sean Pont <seanpont@gmail.com>
2012-11-27 16:58:59 -0800

    now it works

--- src/main/java/com/seanpont/lucene/Indexer.java
+++ src/main/java/com/seanpont/lucene/Indexer.java
@@ -4,30 +4,32 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.IndexFileNames;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
+/**
+ * Indexer that indexes .txt files.
+ * From Lucene in action, Second Edition
+ * @author seanpont
+ */
 public class Indexer {
 
-    
     private IndexWriter writer;
     
     public Indexer(String indexDir) throws IOException {
         Directory dir = FSDirectory.open(new File(indexDir));
         
-        Analyzer analyzer = null;
+        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
         
-        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer );
+        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
         writer = new IndexWriter(dir, config);
     }
     
@@ -37,12 +39,17 @@ public class Indexer {
     
     
     public int index(String dataDir, FileFilter filter) throws Exception {
-        File[] files  = new File(dataDir).listFiles();
+        File dataFile = new File(dataDir);
+        assert dataFile.exists();
+        System.out.println(dataFile.getAbsolutePath()); 
+        
+        File[] files  = dataFile.listFiles();
         for (File f : files) {
-            if (!f.isDirectory() &&)!f.isHidden() &&
-            f.exists() &&
-            f.canRead() &&
-            (filter == null || filter.accept(f))) {
+            if (!f.isDirectory() &&
+                    !f.isHidden() &&
+                    f.exists() &&
+                    f.canRead() &&
+                    (filter == null || filter.accept(f))) {
                 indexFile(f);
             }
         }
@@ -58,19 +65,23 @@ public class Indexer {
     
     protected Document getDocument(File f) throws Exception {
         Document doc = new Document();
-        doc.add(new TextField("contents", new FileReader(f)));
+        doc.add(new Field("contents", new FileReader(f)));
         doc.add(new Field("filename", f.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-        
-        
-        return null;
+        doc.add(new Field("fullpath", f.getCanonicalPath(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+        return doc;
     }
     
-    
+    private void indexFile(File f) throws Exception {
+        System.out.println("Indexing "+f.getCanonicalPath());
+        Document doc = getDocument(f);
+        writer.addDocument(doc);
+    }
     
     public static void main(String[] args) throws Exception {
         //parsing args
         if (args.length != 2) {
-            throw new IllegalArgumentException("Usage: java "+Indexer.class.getName() + " <index dir> <data dir>");
+            args = new String[] {"C:/workspace/learn-lucene/src/main/resources/index", "C:/workspace/learn-lucene/src/main/resources/docs"};
+//            throw new IllegalArgumentException("Usage: java "+Indexer.class.getName() + " <index dir> <data dir>");
         }
         
         String indexDir = args[0];
